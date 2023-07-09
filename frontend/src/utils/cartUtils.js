9766ed6b4b7c263160db45baa103120cbe2b3e86
Soheil86 <soheilghanbari1365@gmail.com>
2023-07-09 22:10:32 +0200

    adding reduce *fixing issue

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -3,23 +3,25 @@ export const addDecimals = (num) => {
 }
 
 export const updateCart = (state) => {
-	//Calculate items price
+	// Calculate the items price
 	state.itemsPrice = addDecimals(
-		state.cartItems((acc, item) => acc + item.price * item.qty, 0)
+		state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
 	)
 
-	// Calculate shipping price
+	// Calculate the shipping price
 	state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
 
-	// Calculate Tax price
+	// Calculate the tax price
 	state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
 
-	// Calculate Total price
+	// Calculate the total price
 	state.totalPrice = (
 		Number(state.itemsPrice) +
-		Number(state.shippingPrice + Number(state.taxPrice))
+		Number(state.shippingPrice) +
+		Number(state.taxPrice)
 	).toFixed(2)
 
+	// Save the cart to localStorage
 	localStorage.setItem('cart', JSON.stringify(state))
 
 	return state
