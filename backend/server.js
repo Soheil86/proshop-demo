1dd9d86af2a08b1bfc168dc21d8f9fbf72c908a8
Soheil86 <soheilghanbari1365@gmail.com>
2023-07-29 20:15:04 +0200

    fixing bugs in middleware

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
-dotenv.config()
 import cookieParser from 'cookie-parser'
+dotenv.config()
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-import { errorHandler, notfound } from './middleware/errorMiddleware.js'
+const port = process.env.PORT || 5000
 
-const port = process.env.PORT || 5001
-
-connectDB() // Connect to MongoDB
+connectDB()
 
 const app = express()
 
-//Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// Cookie parser middleware
 app.use(cookieParser())
 
 app.use('/api/products', productRoutes)
@@ -48,7 +45,9 @@ if (process.env.NODE_ENV === 'production') {
 	})
 }
 
-app.use(notfound)
+app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () =>
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+)
