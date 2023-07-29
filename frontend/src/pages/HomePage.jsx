0c8509329d4152625bcbb253de52bd41022a1f64
Soheil86 <soheilghanbari1365@gmail.com>
2023-07-29 17:02:43 +0200

    implemeting Pagination for user and also Admin User in productListPage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import { Row, Col } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 
 const HomePage = () => {
-	const { data: products, isLoading, error } = useGetProductsQuery()
+	const { pageNumber } = useParams()
+	const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
 
 	return (
 		<>
@@ -19,12 +22,13 @@ const HomePage = () => {
 				<>
 					<h1>Latest Products</h1>
 					<Row>
-						{products.map((product) => (
+						{data.products.map((product) => (
 							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 								<Product product={product} />
 							</Col>
 						))}
 					</Row>
+					<Paginate pages={data.pages} page={data.page} />
 				</>
 			)}
 		</>
--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -4,7 +4,7 @@ import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
-// import Paginate from '../../components/Paginate'
+import Paginate from '../../components/Paginate'
 import {
 	useGetProductsQuery,
 	useDeleteProductMutation,
@@ -15,7 +15,9 @@ import { toast } from 'react-toastify'
 const ProductListPage = () => {
 	const { pageNumber } = useParams()
 
-	const { data: products, isLoading, error, refetch } = useGetProductsQuery({})
+	const { data, isLoading, error, refetch } = useGetProductsQuery({
+		pageNumber,
+	})
 
 	const [deleteProduct, { isLoading: loadingDelete }] =
 		useDeleteProductMutation()
@@ -79,7 +81,7 @@ const ProductListPage = () => {
 							</tr>
 						</thead>
 						<tbody>
-							{products.map((product) => (
+							{data.products.map((product) => (
 								<tr key={product._id}>
 									<td>{product._id}</td>
 									<td>{product.name}</td>
@@ -104,7 +106,7 @@ const ProductListPage = () => {
 							))}
 						</tbody>
 					</Table>
-					{/* <Paginate pages={data.pages} page={data.page} isAdmin={true} /> */}
+					<Paginate pages={data.pages} page={data.page} isAdmin={true} />
 				</>
 			)}
 		</>
