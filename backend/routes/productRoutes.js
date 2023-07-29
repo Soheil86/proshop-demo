a38ec66990d017d1ecd73c8296be15e2e4370bbe
Soheil86 <soheilghanbari1365@gmail.com>
2023-07-29 16:03:56 +0200

    Adding Review features to productlistPage ,then user can write Review for each product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
 	getProductById,
 	getProducts,
 	updateProduct,
+	createProductReview,
 } from '../controllers/productController.js'
 const router = express.Router()
 import { protect, admin } from '../middleware/authMiddleware.js'
@@ -15,5 +16,7 @@ router
 	.get(getProductById)
 	.put(protect, admin, updateProduct)
 	.delete(protect, admin, deleteProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
